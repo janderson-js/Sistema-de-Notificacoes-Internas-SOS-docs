0a4371764a6c444f4e936ec5b81c3a27e5b99472
Janderson Souza <76784547+janderson-js@users.noreply.github.com>
2026-09-17 21:23:44 -0300

    Add example Firebase configuration file

--- extensão/content/firebase-config-exemple.js
+++ extensão/content/firebase-config-exemple.js
@@ -6,10 +6,10 @@
 // ============================================================
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC03SpiPS6bfNJ442LMoRS2hmuiQ7O7Y5g",
-  authDomain: "painel-de-notificacao.firebaseapp.com",
-  projectId: "painel-de-notificacao",
-  storageBucket: "painel-de-notificacao.firebasestorage.app",
-  messagingSenderId: "309434470083",
-  appId: "1:309434470083:web:d5bb8c6ffb7c5ea559dc76"
+  apiKey: "SUA_API_KEY",
+  authDomain: "seu-projeto.firebaseapp.com",
+  projectId: "seu-projeto",
+  storageBucket: "seu-projeto.firebasestorage.app",
+  messagingSenderId: "SEU_ID",
+  appId: "SEU_APP_ID"
 };
